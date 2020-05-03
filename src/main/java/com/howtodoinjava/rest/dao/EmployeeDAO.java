fabd2632237d4642b40dea51657b78becf40368d
yogain19 <yogendrasaxena56@gmail.com>
2020-05-03 21:21:16 +0530

    updated data

--- src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
+++ src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
@@ -6,22 +6,19 @@ import com.howtodoinjava.rest.model.Employee;
 import com.howtodoinjava.rest.model.Employees;
 
 @Repository
-public class EmployeeDAO 
-{
+public class EmployeeDAO {
     private static Employees list = new Employees();
-    
-    static 
-    {
-        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
-        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+
+    static {
+        list.getEmployeeList().add(new Employee(1, "yogendra", "Saxena", "yogendrasaxena@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Rohit", "Singh", "rohitsingh@gmail.com"));
         list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
     }
-    
-    public Employees getAllEmployees() 
-    {
+
+    public Employees getAllEmployees() {
         return list;
     }
-    
+
     public void addEmployee(Employee employee) {
         list.getEmployeeList().add(employee);
     }
